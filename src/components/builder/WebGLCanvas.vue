c9c9bcd2df938604e11a49c37a00a5431a1c5b3c
onoeserika <984426676@qq.com>
2024-02-06 19:10:29 +0800

    wanghuimin 2024/02/06 0

---
+++ src/components/builder/WebGLCanvas.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="absolute w-full h-screen block p-0 m-0">
+        <canvas
+            class="w-full h-full"
+            id="backgroundgl"
+            ref="canvas"
+        />
+            <!-- @pointermove="onPointerMove"
+            @pointerdown="onPointerDown"
+            @pointerup="onPointerUp"/> -->
+    </div>
+</template>
+
+<script setup lang="ts">
+import { inject, onBeforeUnmount, onMounted, onUnmounted, ref } from 'vue';
+
+const setup = ref(false);
+const canvas = ref(null as unknown as HTMLCanvasElement);
+
+onMounted(async () => {
+
+});
+
+onBeforeUnmount(async() => {
+
+});
+
+const frame = () => {
+    if(!setup.value) return;
+    
+}
+
+</script>
---
+++ src/scripts/builder/render/three_wrapper.ts
@@ -0,0 +1,25 @@
+export * as ALL_THREE from 'three';
+
+export * as SkeletonUtils from 'three/examples/jsm/utils/SkeletonUtils.js';
+
+export { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+export { FlyControls } from 'three/examples/jsm/controls/FlyControls.js';
+export { SelectionBox } from 'three/examples/jsm/interactive/SelectionBox.js';
+export * as BufferGeometryUtils from 'three/examples/jsm/utils/BufferGeometryUtils.js';
+
+export { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+export { EXRLoader } from 'three/examples/jsm/loaders/EXRLoader.js';
+
+export { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+export { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+export { SAOPass } from 'three/examples/jsm/postprocessing/SAOPass.js';
+// export { SSAOPass } from '@/builder/graphics/BetterSSAOPass.js';
+export { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
+// export { SSAARenderPass } from '@/builder/graphics/SSAARenderPass.js';
+
+export { SMAAPass } from 'three/examples/jsm/postprocessing/SMAAPass.js';
+export { CopyShader } from 'three/examples/jsm/shaders/CopyShader.js';
+export { GammaCorrectionShader } from 'three/examples/jsm/shaders/GammaCorrectionShader.js';
+export { FXAAShader } from 'three/examples/jsm//shaders/FXAAShader.js';
+
+// export { TAARenderPass } from '@/builder/graphics/TAA.js';
